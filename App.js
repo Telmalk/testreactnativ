73b92cf9aa9b3a184a3d2f2030eb4563503d0f74
root <root@user.user>
2019-03-29 18:15:54 +0100

    true react nat

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 // App.js
 import React from 'react'
-import Navigation from './Navigation/Navigation'
+import Navigation from "./Navigation/Navigation"
 import { Provider } from 'react-redux'
 import Store from './Store/configureStore'
-
+import { Text } from 'react-native'
+import Search from "./comonents/Test"
 export default class App extends React.Component {
   render() {
     return (
-      <Provider store={Store}>
-        <Navigation />
-      </Provider>
+      <Search />
+     // <Text>Coucou</Text>
+    //  <Provider store={Store}>
+      //  <Navigation />
+      //</Provider>
     )
   }
 }
